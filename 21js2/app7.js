1a0a48f1eb8a51c59216dc975830055366693257
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-13 20:32:57 +0530

    practice 3

---
+++ 21js2/app7.js
@@ -0,0 +1,21 @@
+// ---logical Operator
+// ----And operator
+let marks = 90;
+// if(marks>=33 && marks>=80 ){
+    //     console.log("pass");
+    //     console.log("A+");
+    // }
+    
+    
+// ----OR operator
+if(marks>=33 ||  marks>=80 ){
+    console.log("pass");
+    console.log("A+");
+}
+
+// ----NOT operator
+if(!(marks>=33) ){
+    console.log("pass");
+    console.log("A+");
+}
+
---
+++ 21js2/practice3.js
@@ -0,0 +1,7 @@
+let str = "snake";
+
+if(str.length>3 && str[0]==='a'){
+    console.log("good string");
+}else{
+    console.log("not a good string");
+}
