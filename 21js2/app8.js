bac1294228ef7ff2c4245865ee813491dc474f0e
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-13 20:41:56 +0530

    practice 4

---
+++ 21js2/app8.js
@@ -0,0 +1,18 @@
+let color = "red";
+
+switch (color) {
+    case "red":
+        console.log("Stop");
+        break;
+    case "yellow":
+        console.log("ready");
+        break;
+    case "green":
+        console.log("go");
+        break;
+
+    default:
+        console.log("broken light");
+        
+        break;
+}
--- 21js2/tempCodeRunnerFile.js
+++ 21js2/tempCodeRunnerFile.js
@@ -1,2 +1,2 @@
 
-console.log(a % b)
+        break;
