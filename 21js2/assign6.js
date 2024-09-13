d837de10ddadab85c86f3863451b1a722344df74
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-13 21:40:10 +0530

    21 js 2 complete

---
+++ 21js2/assign6.js
@@ -0,0 +1,12 @@
+let num1 = '32';
+let num2 = '47852';
+
+// let lg1 = num1.length;
+
+if (num1[num1.length -1] === num2[num2.length -1]) {
+    console.log("it has same last digit which is :");
+    console.log(num1[num1.length -1]);
+} else {
+    console.log("it does not have same last digit ");
+    
+}
