b9c21d5964dfcd24d68b6c05d5835ad538410a58
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-13 19:21:39 +0530

    operators

---
+++ 20js2/app3.js
@@ -0,0 +1,17 @@
+let a = 10;
+let b = 5;
+
+// arithmetic Operators
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+// unary operators
+console.log(++a);
+console.log(a++);
+
+// b = a;
+// console.log(b);
