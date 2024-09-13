1c6722007607f34d21052bc05972ab58ef32f5cb
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-13 20:20:47 +0530

    practice 2

---
+++ 20js2/app5.js
@@ -0,0 +1,11 @@
+let age = 14;
+
+if (age >= 18) {
+    console.log("you can vote");
+} else if(age === 14){
+    console.log("you are teen");
+    
+}else{
+    console.log("you cannot vote");
+
+}
