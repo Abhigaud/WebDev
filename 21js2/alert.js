1b5734f99141abdfb703adacf2690751eea8770a
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-13 20:50:32 +0530

    alert and prompts

---
+++ 21js2/alert.js
@@ -0,0 +1,8 @@
+// alert("something is wrong");
+console.log("this is a simple log");
+console.error("this is a simple error msg");
+console.warn("this is a simple warning msg");
+
+let firstName = prompt("Enter your first name: ");
+let lastName = prompt("Enter your last name: ");
+console.log(`Welcome ${firstName} ${lastName} !`);
