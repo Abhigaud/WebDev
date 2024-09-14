5e6f5d9f5613bc28b45f1dc55df7f8139ca29d90
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-14 22:03:56 +0530

    method chaining

--- 22js3/app2.js
+++ 22js3/app2.js
@@ -1,4 +1,8 @@
 // --- indexOf()
 let str = "ILoveCoding";
-
 console.log(str.indexOf("ove"));
+
+// -------method Chaining
+let msg = "    hello      ";
+let newMsg = msg.trim().toUpperCase();
+console.log(newMsg);
