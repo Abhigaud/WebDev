0749763ca07bebe7a8a9b021612ef3a0aa20c1b0
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-14 21:56:50 +0530

    .toLowerCase()

--- 22js3/c1_trimMethod.js
+++ 22js3/c1_trimMethod.js
@@ -2,3 +2,9 @@ let password = prompt("set your password: ");
 let newPass = password.trim();
 console.log(password);
 
+// toUpperCase();
+console.log(newPass.toUpperCase());
+
+let wrd = "WERTWE";
+// .toLowerCase()
+console.log(wrd.toLowerCase());
