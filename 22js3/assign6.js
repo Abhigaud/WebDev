6efbf3777709d396dd8eb609f7679081cda63649
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-15 23:32:00 +0530

    for loop

---
+++ 22js3/assign6.js
@@ -0,0 +1,10 @@
+let arr = ['a','b','c','d','e','f'];
+let elm = 'd'
+console.log(arr.indexOf(elm));
+
+if (arr.indexOf(elm) != -1) {
+    console.log("element exists in array");
+} else {
+    console.log("element doesn't exists in array");
+}
+
