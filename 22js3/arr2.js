9bcaea1a9f744d5ab675351b727dc342f64ee6f6
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-15 21:50:59 +0530

    .push

---
+++ 22js3/arr2.js
@@ -0,0 +1,8 @@
+let cars = ["audi", "bmw", "XUV", "Maruti"];
+console.log(cars);
+
+cars.push("toyota");
+console.log(cars);
+
+cars.pop();
+console.log(cars);
---
+++ 22js3/array.js
@@ -0,0 +1,9 @@
+let students = ["aman","shraddha", "rajat" ];
+console.log(students);
+
+let nums = [4,2,5,6]
+console.log(nums[2]);
+console.log(nums.length);
+console.log(typeof nums);
+
+let newArr = [];
