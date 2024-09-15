a1679f73a2a9d4e8a98ecc6113c66467d4359425
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-15 22:08:05 +0530

    slice in array

--- 22js3/arr3.js
+++ 22js3/arr3.js
@@ -8,8 +8,14 @@ console.log(primary.includes("audi"));
 console.log(primary.includes("yellow"));
 
 let secondary = ["orange", "green", "violet"];
-let allCol = primary.concat(secondary);
-console.log(allCol);
+let colors = primary.concat(secondary);
+console.log(colors);
 
 secondary.reverse();
 console.log(secondary);
+
+console.log(colors.slice());
+console.log(colors.slice(2));
+console.log(colors.slice(2,4));
+console.log(colors.slice(-2));
+
