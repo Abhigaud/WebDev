02bc4e479c7973a89d32bc99942256b513cc2e97
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-15 22:00:06 +0530

    indexOf, includes

---
+++ 22js3/arr3.js
@@ -0,0 +1,8 @@
+let primary = ["red", "yellow", "blue"];
+
+console.log(primary.indexOf("red"));
+console.log(primary.indexOf("blue"));
+console.log(primary.indexOf("Blue"));
+
+console.log(primary.includes("audi"));
+console.log(primary.includes("yellow"));
