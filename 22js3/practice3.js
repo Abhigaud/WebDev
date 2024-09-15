4fdd2b5dce7a25f0e97c34c0ce6f85306fc176a1
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-15 22:49:13 +0530

    practice 3

---
+++ 22js3/practice3.js
@@ -0,0 +1,7 @@
+let months = ['january', 'july', 'march', 'august'];
+months.splice(0,2, 'july', 'june');
+console.log(months);
+
+let tech = ["c", "c++", "html", "javascript", "python", "java", "c#", "sql"];
+tech.reverse();
+console.log(tech.indexOf("javascript"));
