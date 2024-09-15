93b8b12d910d6135aa85bcb341b550c020ace403
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-15 23:11:12 +0530

    assignment 2

---
+++ 22js3/assign2.js
@@ -0,0 +1,5 @@
+let arr1 = [7,9,0,-2];
+let n =3;
+
+// console.log(arr1.slice(-n));
+console.log(arr1.slice(arr1.length - n));
---
+++ 22js3/assign3.js
@@ -0,0 +1,5 @@
+let str = prompt("Please enter a string");
+if (str.length == 0) {
+    console.log("String is empty");
+       
+}
