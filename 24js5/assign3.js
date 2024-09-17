5deabc4a8d255c6c2bc23e9f0c92e0595d171896
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-17 09:20:37 +0530

    Assignment 3, js_P5

---
+++ 24js5/assign3.js
@@ -0,0 +1,11 @@
+const person = {
+    name : "abhishek",
+    age : 22,
+    city : "Delhi"
+}
+console.log(person.city);
+person.city = "New York";
+console.log(person.city);
+person.country = "United States";
+console.log(person.country);
+
