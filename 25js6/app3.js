7f96c09112fecba55acf155adfae579e710e4018
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-17 09:54:39 +0530

    practice 5 , 25js6

---
+++ 25js6/app3.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+    let total = a+b;
+    return total;
+}
+
+console.log(sum(8,3));
---
+++ 25js6/practice5.js
@@ -0,0 +1,9 @@
+function sum1toN(n){
+    let sum = 0;
+    for (let i = 1; i <=n; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(sum1toN(900));
