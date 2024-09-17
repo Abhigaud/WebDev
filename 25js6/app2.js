3871553428476d9c17d52c8fd1e7ffc8d198bc5f
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-17 09:42:22 +0530

    practice3, 25js6

---
+++ 25js6/app2.js
@@ -0,0 +1,15 @@
+function printName(name){
+    console.log(name);
+}
+function printInfo(name, age){
+    console.log(`${name}'s age is ${age}`);
+}
+
+printName("Abhi");
+printInfo("annu", 22);
+printInfo("annu");
+
+function sum(a, b) {
+    console.log(a+b);
+}
+sum(2,3);
---
+++ 25js6/practice3.js
@@ -0,0 +1,6 @@
+function avgOf3(a,b,c) {
+    let avg = (a+b+c)/3;
+    console.log(avg);
+}
+
+avgOf3(33,11,22);
