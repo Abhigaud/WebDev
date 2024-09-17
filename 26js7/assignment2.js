5caaba435cabfbe55170a1e11d1e3d323c5e75cb
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-18 00:46:45 +0530

    26js7 complete

---
+++ 26js7/assignment2.js
@@ -0,0 +1,9 @@
+const isEven = (n) => {
+    if (n % 2 == 0) {
+        return "even";
+    }else{
+        return "not even";
+    }
+};
+
+console.log(isEven(7));
---
+++ 26js7/assignment3.js
@@ -0,0 +1,6 @@
+const object = {
+    message : 'Hello, World!', logMessage(){ 
+        console.log(this.message);
+    }
+};
+setTimeout(object.logMessage, 1000);
