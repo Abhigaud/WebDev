a0c3375835f02ebc48544eff6e9ede932215e62a
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-17 09:33:31 +0530

    practice1, 25js6

---
+++ 25js6/app1.js
@@ -0,0 +1,30 @@
+function hello() {
+    console.log("hello");
+}
+hello();
+hello();
+hello();
+
+function printName() {
+    console.log("apna college");
+    console.log("shradha Khapra");
+}
+printName();
+
+function print1to5() {
+    for (let i = 0; i <= 5; i++) {
+        console.log(i);
+    }
+}
+print1to5();
+
+function isAdult(age) {
+    if (age >= 18) {
+        console.log("adult");
+    }else{
+        console.log("not adult");
+    }
+}
+
+isAdult(23);
+isAdult(3);
---
+++ 25js6/practice1.js
@@ -0,0 +1,8 @@
+function poem() {
+    console.log("Twinkle Twinkle, little str");
+    console.log("How I wonder what you are");
+    console.log("up above the world so high");
+    console.log("like a diamond in the sky");
+}
+
+poem();
