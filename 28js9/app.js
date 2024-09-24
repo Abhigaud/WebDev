24ac506a33bd26a0b0998a4fb986f943e87a0f08
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-24 21:42:12 +0530

    24/9

--- 28js9/app.js
+++ 28js9/app.js
@@ -1,8 +1,19 @@
-let smallImages = document.getElementsByClassName("oldImg");
+// let smallImages = document.getElementsByClassName("oldImg");
 
-for (let i = 0; i < smallImages.length; i++) {
-    // console.dir(smallImages[i].src);
-    smallImages[i].src = "assets/spiderman_img.png";
-    console.log(`value of image no. ${i} is changed`);
-    
-}
+// for (let i = 0; i < smallImages.length; i++) {
+//     // console.dir(smallImages[i].src);
+//     smallImages[i].src = "assets/spiderman_img.png";
+//     console.log(`value of image no. ${i} is changed`);   
+// }
+
+// query selector-----------------------------
+
+// console.dir(document.querySelector("h1"));
+// console.dir(document.querySelector("#description"));
+// console.dir(document.querySelector(".oldImg"));
+// console.dir(document.querySelector("div a"));
+
+
+// query selector-----------------------------
+console.dir(document.querySelectorAll("div a"));
+console.dir(document.querySelectorAll("p"));
