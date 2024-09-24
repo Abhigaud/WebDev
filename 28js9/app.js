75bf1d839f67e682d768a838626c7647b2e7f366
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-24 20:21:50 +0530

    28js9 start

---
+++ 28js9/app.js
@@ -0,0 +1,8 @@
+let smallImages = document.getElementsByClassName("oldImg");
+
+for (let i = 0; i < smallImages.length; i++) {
+    // console.dir(smallImages[i].src);
+    smallImages[i].src = "assets/spiderman_img.png";
+    console.log(`value of image no. ${i} is changed`);
+    
+}
