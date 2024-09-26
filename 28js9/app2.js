b72ca71e78731776ff499735b3f9fa475cf6ef3b
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-26 22:12:07 +0530

    adding elements

--- 28js9/app2.js
+++ 28js9/app2.js
@@ -15,3 +15,37 @@ console.log(ul.children[1]);
 console.log(ul.children[2]);
 console.log(ul.children[2].previousElementSibling);
 console.log(ul.children[0].nextElementSibling);
+
+
+
+//--------- adding elements -----------------------------------------
+console.log(document.createElement('p'));
+let newP = document.createElement('p');
+console.dir(newP);
+newP.innerText = "hi , I am a new p";
+let body = document.querySelector('body');
+body.appendChild(newP);
+box.appendChild(newP);
+
+let btn = document.createElement('button');
+console.dir('btn');
+
+btn.innerText = 'click me!';
+box.appendChild(btn);
+
+newP.append("this is new text");
+newP.append(btn);
+newP.append("this is new btn");
+
+box.prepend(newP)
+
+let p = document.querySelector('p');
+let btn1 = document.createElement('button');
+btn1.innerHTML = "new Button!!!";
+p.insertAdjacentElement('beforebegin', btn1);
+p.insertAdjacentElement('afterbegin', btn1);
+p.insertAdjacentElement('beforeend', btn1);
+p.insertAdjacentElement('afterend', btn1);
+
+body.insertAdjacentElement("afterend", btn1);
+btn1.remove();
