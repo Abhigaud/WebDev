b47f679d75b6fce0a4f4df081fcff7f275768c60
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-30 22:43:02 +0530

    28js10 comp

---
+++ 28js10/assign1.js
@@ -0,0 +1,20 @@
+let btn = document.querySelector("button");
+let input = document.querySelector("input");
+
+//// ------------- mouseout ------------------------------------
+// btn.addEventListener("mouseout" , function () {
+//     alert("mouseout");
+// });
+
+
+// // ------------- keypress -----------------------------------
+// input.addEventListener("keypress" , function () {
+//     alert("keypress");
+// });
+
+// // ------------- Scroll -----------------------------------
+function doSomething(scrollPos) {
+  alert("do something");
+}
+
+p.addEventListener("scroll", doSomething);
---
+++ 28js10/assign2.js
@@ -0,0 +1,21 @@
+let btn = document.querySelector("button");
+
+//  ------------------------------------
+btn.addEventListener("click" , function () {
+    btn.style.backgroundColor = "green";
+});
+
+// -----------------------------------------
+const nameInput = document.getElementById('nameInput');
+const nameDisplay = document.getElementById('nameDisplay');
+
+nameInput.addEventListener('input', () => {
+  const inputValue = nameInput.value;
+  const validNameRegex = /^[a-zA-Z ]+$/;
+
+  if (validNameRegex.test(inputValue)) {
+    nameDisplay.textContent = inputValue;
+  } else {
+    nameDisplay.textContent = inputValue.replace(/[^a-zA-Z ]/g, '');
+  }
+});
