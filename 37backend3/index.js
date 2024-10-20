9347020daaacdc1797e7031b26e90b5cb48ac9ed
Abhigaud <abhishekgaud.annu@gmail.com>
2024-10-20 23:44:27 +0530

    instagram ejs

--- 37backend3/index.js
+++ 37backend3/index.js
@@ -18,10 +18,25 @@ app.get("/hello", (req, res)=>{
     
 });
 
+// app.get("/ig/:username", (req, res)=>{
+//     const followers = ["rajesh","mahesh", "shiva", "loki" ];
+//     let {username} = req.params;
+//     res.render("instagram.ejs", {username, followers});
+// });
+
+app.get("/ig/:username", (req, res)=>{
+    let {username} = req.params;
+    const instaData = require("./data.json");
+    const data = instaData[username];
+    console.log(data);
+    
+    res.render("instagram1.ejs", {data});
+});
+
 app.get("/rolldice", (req, res)=>{
     // res.send("hello");
     let diceVal = Math.ceil(Math.random()*6);
-    res.render("rolldice.ejs", {num:diceVal});
+    res.render("rolldice.ejs", {diceVal});
 });
 
 app.listen(port, ()=>{
