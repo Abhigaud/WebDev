cc95a73bd6fb3d9ced32ec0a4ee37af22053dc7e
Abhigaud <abhishekgaud.annu@gmail.com>
2024-10-21 19:44:33 +0530

    before includes

--- 37backend3/index.js
+++ 37backend3/index.js
@@ -1,36 +1,44 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
 const port = 8080;
+
+// app.set(express.static("public"));
+app.use(express.static(path.join(__dirname,"/public/css")));
+app.use(express.static(path.join(__dirname,"/public/js")));
 
 // -----using EJS----------------
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"/views"))
 
 app.get("/", (req, res)=>{
-    // res.send("this is home ");
     res.render("home.ejs");
 });
-
-app.get("/hello", (req, res)=>{
-    res.send("hello");
-    
-});
-
-// app.get("/ig/:username", (req, res)=>{
-//     const followers = ["rajesh","mahesh", "shiva", "loki" ];
-//     let {username} = req.params;
-//     res.render("instagram.ejs", {username, followers});
-// });
 
 app.get("/ig/:username", (req, res)=>{
     let {username} = req.params;
     const instaData = require("./data.json");
     const data = instaData[username];
-    console.log(data);
+    // console.log(data);
+    if (data) {
+        res.render("instagram.ejs", {data});
+    }else{
+        res.render("err.ejs");
+    }
+    res.render("instagram.ejs", {data});
+});
+
+
+// app.get("/ig/:username", (req, res)=>{
+//     const followers = ["rajesh","mahesh", "shiva", "loki" ];
+//     let {username} = req.params;
+//     res.render("instagram1.ejs", {username, followers});
+// });
     
-    res.render("instagram1.ejs", {data});
+    
+app.get("/hello", (req, res)=>{
+    res.send("hello");
+        
 });
 
 app.get("/rolldice", (req, res)=>{
