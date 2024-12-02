4eb117709b9f136f290b03db0f220025b1a9cccc
Abhigaud <abhishekgaud.annu@gmail.com>
2024-12-02 13:27:01 +0530

    38miscellaneous comp

--- 38miscellaneous/backend/index.js
+++ 38miscellaneous/backend/index.js
@@ -1,22 +1,21 @@
-const express = require("express");
-const app = express();
-const port = 8080;
+ const express = require("express");
+ const app = express();
+ const port = 8080;
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+ app.use(express.urlencoded({extended:true}));
+ app.use(express.json());
 
 
-app.get("/register",(req,res) => {
+ app.get("/register", (req, res) => {
     let {user, password} = req.query;
-    res.send(`standard GET response. Welcome ${user}!`);
-});
+    res.send(`standard GET response, welcome ${user}`);
+ });
 
-app.post("/register",(req,res) => {
+ app.post("/register", (req, res) => {
     let {user, password} = req.body;
-    // console.log(req.body);
-    res.send(`standard POST response. Welcome ${user}!`);
-});
+    res.send(`standard GET response, welcome ${user}`);
+ });
 
-app.listen(port, ()=>{
-    console.log(`listening to port ${port} `); 
-});
+ app.listen(port, ()=>{
+    console.log(`listening to port ${port}`);
+ });
