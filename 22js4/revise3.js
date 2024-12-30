7e9660d7a2624131a2dd487af57cc9d686a8036a
Abhigaud <abhishekgaud.annu@gmail.com>
2024-12-30 08:00:58 +0530

    23js5 revise

---
+++ 22js4/revise3.js
@@ -0,0 +1,21 @@
+let dice = Math.floor(Math.random()*6)+1;
+console.log(dice);
+
+let Car = {
+    name : "XUV",
+    model : 700,
+    color: "black"
+};
+
+console.log(Car.name);
+
+
+let person = {
+    name : "Abhishek",
+    Age : "24",
+    city : "Gwalior"
+}
+console.log(person);
+person.city = "New York";
+person.country = "United States"
+console.log(person);
