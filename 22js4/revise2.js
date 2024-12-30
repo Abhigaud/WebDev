e02a7be6a1717d9c22be4a725259116e7e1cfec3
Abhigaud <abhishekgaud.annu@gmail.com>
2024-12-30 07:52:42 +0530

    guessing game revise

--- 22js4/revise2.js
+++ 22js4/revise2.js
@@ -1,11 +1,32 @@
-let num = Math.random();
-num = num *10;
+// let num = Math.random();
+// num = num *10;
 
-num = Math.floor(num);
-num = num + 1;
-console.log(num);
+// num = Math.floor(num);
+// num = num + 1;
+// console.log(num);
 
-let random = (Math.random()*100) + 1;
-console.log(random);
+let max = prompt("Enter the max number");
 
+let random = Math.floor(Math.random()*max) + 1;
+
+let guess = prompt("guess the number");
+
+// console.log(random);
+while (true) {
+    if (guess == "quit") {
+        console.log("User quit");
+        break
+    }
+    if (guess == random) {
+        console.log("you are right!");
+        break;
+    }else if (guess < random) {
+        guess = prompt("your guess was too small, please try again");
+    }else if(guess > random){
+        guess = prompt("your guess was large, please try again");
+    }
+    // else{
+    //     guess = prompt("your guess was wrong, Please try again");
+    // }
+}
  
