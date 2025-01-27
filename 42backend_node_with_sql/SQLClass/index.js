22ebeede108d8881be88732cc142902a997a51e3
Abhigaud <abhishekgaud.annu@gmail.com>
2025-01-27 13:10:14 +0530

    Home route and show route

--- 42backend_node_with_sql/SQLClass/index.js
+++ 42backend_node_with_sql/SQLClass/index.js
@@ -1,5 +1,12 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require ('mysql2');
+const express = require("express");
+const app = express();
+const path = require("path");
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -32,28 +39,64 @@ let getRandomUser = () => {
     //     ["123c", "123_newuserc", "abc@gmail.comc", "abcc"]
     // ];
     
-// --------------------------------------------------
-// insert bulk data with faker
-let q = "INSERT INTO user (id, username, email, password) VALUES ?";
-let data = [];
-for (let i = 1; i <= 100; i++) {
-    data.push(getRandomUser());
+// // --------------------------------------------------
+// // insert bulk data with faker
+// let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+// let data = [];
+// for (let i = 1; i <= 100; i++) {
+//     data.push(getRandomUser());
     
-}
+// }
 
-try{
-    connection.query(q, [data] ,(err, result) => {
-        if(err) throw err;
-        console.log(result);
-    });
-}catch (err){
-    console.log(err);
-}
 
-connection.end();
+// try{
+//     connection.query(q, [data] ,(err, result) => {
+//         if(err) throw err;
+//         console.log(result);
+//     });
+// }catch (err){
+//     console.log(err);
+// }
+
+// connection.end();
 
 
 //   console.log(getRandomUser());
 
+// Home page route
+app.get("/", (req, res) => {
+    let q = `SELECT count(*) FROM user`;
+    try{
+            connection.query(q, (err, result) => {
+                if(err) throw err;
+                let count = result[0]["count(*)"];
+                // res.send(result);
+                // res.send("success");
+                res.render("home.ejs", {count});
+            });
+        }catch (err){
+            console.log(err);
+            res.send("some error in DB");
+        }
+});
 
-  
+// Show route
+app.get("/user", (req, res) => {
+    let q = `SELECT * FROM user`;
+    try{
+            connection.query(q, (err, users) => {
+                if(err) throw err;
+                
+                // res.send(users);
+                res.render("showusers.ejs", {users}) ;
+            });
+        }catch (err){
+            console.log(err);
+            res.send("some error in DB");
+        }
+});
+
+  app.listen("8080", () => {
+    console.log("server is listening to port 8080");
+    
+  })
