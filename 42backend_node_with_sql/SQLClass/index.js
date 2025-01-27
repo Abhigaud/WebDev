dc8d5367cf57a61a80b99863a1388749c4682721
Abhigaud <abhishekgaud.annu@gmail.com>
2025-01-27 10:01:30 +0530

    insert a single data into Mysql

--- 42backend_node_with_sql/SQLClass/index.js
+++ 42backend_node_with_sql/SQLClass/index.js
@@ -1,4 +1,27 @@
 const { faker } = require('@faker-js/faker');
+const mysql = require ('mysql2');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "delta_app",
+    password: "A6091401g",
+})
+
+// let q = "SHOW TABLES";
+let q = "INSERT INTO user (id, username, email, password) VALUES (?, ?, ?, ?) ";
+let user = ["123", "123_newuser", "abc@gmail.com", "abc"];
+
+try{
+    connection.query(q, user ,(err, result) => {
+        if(err) throw err;
+        console.log(result);
+    });
+}catch (err){
+    console.log(err);
+}
+
+connection.end();
 
 let getRandomUser = () => {
     return {
@@ -9,7 +32,7 @@ let getRandomUser = () => {
     };
   }
 
-  console.log(getRandomUser());
+//   console.log(getRandomUser());
 
 
   
