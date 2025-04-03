fe7b8c2b8f2f1bfcd3b3ba551c1e17fc3f347ab4
Abhigaud <abhishekgaud.annu@gmail.com>
2025-04-03 18:05:00 +0530

    before init.js

---
+++ 46mongoWithExpress/init.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Chat = require("./models/chat.js");
+
+
+main()
+    .then(() => {
+        console.log("connection successful");
+    })
+    .catch((err) => console.log(err));
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+}
